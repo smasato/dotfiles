6d66d26a2dc250d9aa218e09f395a350aed28434
Masato Sugiyama <public@smasato.net>
2021-12-21 03:16:43 +0900

    udpate .chezmoiscripts/run_once_before_10-install-zsh.sh

--- .chezmoiscripts/run_once_before_10-install-zsh.sh
+++ .chezmoiscripts/run_once_before_10-install-zsh.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-brew install zsh
+brew install zsh zsh-completions
 sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
 echo "chsh -s /usr/local/bin/zsh" ; chsh -s /usr/local/bin/zsh
+
+brew tap "romkatv/powerlevel10k"
+brew install "romkatv/powerlevel10k/powerlevel10k"
+brew install peco
+brew install asdf
+brew install exa
