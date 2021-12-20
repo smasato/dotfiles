ac0d8860a2a2050f52c3ae944509425778d9882f
Masato Sugiyama <public@smasato.net>
2021-12-21 03:05:29 +0900

    update .chezmoiscripts/run_once_after_10-install-asdf.sh

--- .chezmoiscripts/run_once_after_10-install-asdf.sh
+++ .chezmoiscripts/run_once_after_10-install-asdf.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/usr/local/bin/zsh
 
-asdf plugin-add nodejs
-asdf plugin-add ruby
-asdf plugin-add python
+installed_plugins=(`asdf plugin-list`)
+plugins=(nodejs ruby python)
+
+for i in $plugins; do
+    if (( ${installed_plugins[(I)$i]} )); then
+        echo $i
+    else
+        asdf plugin-add $i
+    fi
+done
