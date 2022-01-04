3cf4cc0a2fbbefbc973713070ef210fd5d5a3cf1
Masato Sugiyama <public@smasato.net>
2022-01-05 07:22:26 +0900

    install p10k by antigen

--- .chezmoiscripts/run_once_before_10-install-zsh.sh
+++ .chezmoiscripts/run_once_before_10-install-zsh.sh
@@ -4,8 +4,6 @@ brew install zsh zsh-completions
 sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
 echo "chsh -s /usr/local/bin/zsh" ; chsh -s /usr/local/bin/zsh
 
-brew tap "romkatv/powerlevel10k"
-brew install "romkatv/powerlevel10k/powerlevel10k"
 brew install peco
 brew install asdf
 brew install exa
