961ae7ef4e33cb10372e718abb57e3e32e651d04
Masato Sugiyama <public@smasato.net>
2022-05-31 06:16:08 +0900

    arm64

--- .chezmoiscripts/run_once_before_9-install-zsh.sh
+++ .chezmoiscripts/run_once_before_9-install-zsh.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 brew install zsh zsh-completions
-sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
-echo "chsh -s /usr/local/bin/zsh" ; chsh -s /usr/local/bin/zsh
+
+
+if [ `uname -m` = 'arm64' ]; then
+    sudo sh -c 'echo /opt/homebrew/bin/zsh >> /etc/shells'
+    echo "chsh -s /opt/homebrew/bin/zsh" ; chsh -s /opt/homebrew/bin/zsh
+fi
+
+if [ `uname -m` = 'x86_64' ]; then
+    sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
+    echo "chsh -s /usr/local/bin/zsh" ; chsh -s /usr/local/bin/zsh
+fi
 
 brew install peco
 brew install asdf
